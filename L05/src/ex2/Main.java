b8d09db4cfc536eae05f1ad69e00f6a316c772ee
Kaare Skau <kaare3014@gmail.com>
2022-02-22 10:01:19 +0100

    Print person

--- L05/src/ex2/Main.java
+++ L05/src/ex2/Main.java
@@ -5,5 +5,8 @@ public class Main {
 
         Person p1 = new Person("Hans", 21);
         Person p2 = new Person("Ulla", 22);
+
+        System.out.println("Navn: " + p1.getName() + " Alder: " + p1.getAge());
+        System.out.println("Navn: " + p2.getName() + " Alder: " + p2.getAge());
     }
 }
