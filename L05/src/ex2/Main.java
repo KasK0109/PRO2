2ceb5cf30aa3aa63e6381eff60a04f21b82ca482
Kaare Skau <kaare3014@gmail.com>
2022-02-22 10:00:17 +0100

    Initial commit

---
+++ L05/src/ex2/Main.java
@@ -0,0 +1,9 @@
+package ex2;
+
+public class Main {
+    public static void main(String[] args) {
+
+        Person p1 = new Person("Hans", 21);
+        Person p2 = new Person("Ulla", 22);
+    }
+}
